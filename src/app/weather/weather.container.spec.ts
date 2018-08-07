56ebba5f05fcfb95b8a46547e7231eaba3305cd5
flashjpr <flashjpr@gmail.com>
2018-08-07 06:02:04 +0100

    container spec tests

--- src/app/weather/weather.container.spec.ts
+++ src/app/weather/weather.container.spec.ts
@@ -1,19 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { WeatherContainerComponent } from './weather.container';
+import {Store, StoreModule} from '@ngrx/store';
+import {reducers} from './store/reducers';
+import {WeatherState} from './store/reducers/weather.reducers';
+import * as fromStore from './store';
+import {AddWeather} from './store/actions';
 
 describe('WeatherContainerComponent', () => {
   let component: WeatherContainerComponent;
   let fixture: ComponentFixture<WeatherContainerComponent>;
-
+  let store: Store<WeatherState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ WeatherContainerComponent ],
-      imports: [],
+      imports: [StoreModule.forRoot(reducers)],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
+    store = TestBed.get( Store );
   }));
 
   beforeEach(() => {
@@ -22,9 +28,22 @@ describe('WeatherContainerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the weather container component', () => {
     expect(component).toBeTruthy();
   });
 
-  // PLEASE IMPLEMENT MORE TESTS
+  it('should create an observable of type Weather[]', () => {
+    expect(component.cities$).toBeTruthy();
+  });
+
+  describe('citySearch method', () => {
+    it('should dispatch the action with the correct value', () => {
+      spyOn(store, 'dispatch').and.callThrough();
+      const input = 'Seoul';
+      const addWeatherAction: AddWeather = new fromStore.AddWeather(input);
+      component.citySearch(input);
+      expect(store.dispatch).toHaveBeenCalledWith(addWeatherAction);
+    });
+  });
+
 });
