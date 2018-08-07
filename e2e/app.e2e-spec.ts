517ca466becd3524c6cfc9e38a99e23b864ba900
flashjpr <flashjpr@gmail.com>
2018-08-07 06:02:35 +0100

    e2e spec tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import {browser, protractor} from 'protractor';
 
 describe('angular-weather App', () => {
   let page: AppPage;
@@ -7,7 +8,86 @@ describe('angular-weather App', () => {
     page = new AppPage();
   });
 
-  it('e2e are running empty, please implement', () => {
-    expect(page);
+  it('should display the correct heading text', () => {
+    page.navigateTo();
+    expect(page.heading.getText()).toEqual('AgileSphere coding test - The Weather App');
   });
+
+  it('should not allow to click the search button if there is no input', () => {
+    page.navigateTo();
+    expect(page.searchCityBtn.isEnabled()).toBe(false);
+  });
+
+  it('should not allow to click the search button if the input is 1 char long', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('L');
+    expect(page.searchCityBtn.isEnabled()).toBe(false);
+  });
+
+  it('should not allow to click the search button if the input is contains special characters', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('London$');
+    expect(page.searchCityBtn.isEnabled()).toBe(false);
+  });
+
+  it('should not allow to click the search button if the input is contains numbers', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('London 2');
+    expect(page.searchCityBtn.isEnabled()).toBe(false);
+  });
+
+  it('should allow to click the search button if the input contains multiple words', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('New York');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+  });
+
+  it('should show the weather forecast table for a valid input', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('London');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.presenceOf(page.cityOutputTable), 3500);
+  });
+
+  it('should display a city only once if searched more than once', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('London');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.presenceOf(page.cityOutputTable), 3500);
+
+    page.searchInputBox.sendKeys('London');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.textToBePresentInElement(page.cityOutputTable, 'London'), 3500);
+
+    page.searchInputBox.sendKeys('London');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.presenceOf(page.cityOutputTable), 3500);
+
+    page.searchInputBox.sendKeys('London');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.textToBePresentInElement(page.cityOutputTable, 'London'), 3500);
+
+    expect(page.cityOutputTable.$$('tr').count()).toBe(2);
+  });
+
+  it('should show the weather forecast for two cities for a valid inputs', () => {
+    page.navigateTo();
+    page.searchInputBox.sendKeys('London');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.presenceOf(page.cityOutputTable), 3500);
+    expect(page.cityOutputTable.$$('tr').get(1).$$('td').getText()).toContain('London');
+
+    page.searchInputBox.sendKeys('Berlin');
+    expect(page.searchCityBtn.isEnabled()).toBe(true);
+    page.searchCityBtn.click();
+    browser.wait(protractor.ExpectedConditions.textToBePresentInElement(page.cityOutputTable, 'Berlin'), 3500);
+    expect(page.cityOutputTable.$$('tr').get(2).$$('td').getText()).toContain('Berlin');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,15 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  heading = element(by.id('nav'));
+  searchInputBox = element(by.id('city'));
+  searchInputRequiredError = element(by.id('searchInputRequiredError'));
+  searchInputMinLengthError = element(by.id('searchInputMinLengthError'));
+  searchInputInvalidInputError = element(by.id('searchInputInvalidInputError'));
+
+  searchCityBtn = element(by.id('searchCityBtn'));
+  cityOutputTable = element(by.id('cityOutputTable'));
+
   navigateTo() {
     return browser.get('/');
   }
@@ -8,4 +17,5 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
 }
